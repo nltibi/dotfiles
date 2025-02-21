45e47428c4a5e25f89971a026f5cfe3012e7fe74
Tibi Nagy <nltibi@users.noreply.github.com>
2025-02-22 01:41:33 +0200

    nvim: Improve the loading/config of Rust plugins

--- .config/nvim/lua/plugins/rust.lua
+++ .config/nvim/lua/plugins/rust.lua
@@ -3,13 +3,21 @@ return {
     "mrcjkb/rustaceanvim",
     version = "^5", -- Recommended
     lazy = false, -- This plugin is already lazy
+    ft = "rs",
   },
   {
     "saecki/crates.nvim",
     tag = "stable",
-    ft = "toml",
+    event = { "BufRead Cargo.toml" },
     config = function()
-      require("crates").setup()
+      require("crates").setup({
+        lsp = {
+          enabled = true,
+          actions = true,
+          completion = true,
+          hover = true,
+        },
+      })
     end,
   },
 }
