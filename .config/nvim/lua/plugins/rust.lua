b41e8549bc099a4e89f3506f27f3ec2ebf646b9e
Tibi Nagy <nltibi@users.noreply.github.com>
2025-06-27 02:16:38 +0300

    fix(nvim): Update Rust plugin's mason config

--- .config/nvim/lua/plugins/rust.lua
+++ .config/nvim/lua/plugins/rust.lua
@@ -6,16 +6,15 @@ return {
     ft = "rust",
     config = function(_, opts)
       if LazyVim.has("mason.nvim") then
-        local package_path = require("mason-registry").get_package("codelldb"):get_install_path()
-        local codelldb = package_path .. "/extension/adapter/codelldb"
-        local library_path = package_path .. "/extension/lldb/lib/liblldb.dylib"
-        local uname = io.popen("uname"):read("*l")
-        if uname == "Linux" then
-          library_path = package_path .. "/extension/lldb/lib/liblldb.so"
+        local registry = require("mason-registry")
+        if registry.is_installed("codelldb") then
+          local codelldb = vim.fn.exepath("codelldb")
+          local library_path =
+            vim.fn.globpath("$MASON/packages/codelldb/extension/lldb/lib", "liblldb.*", true, true)[1]
+          opts.dap = {
+            adapter = require("rustaceanvim.config").get_codelldb_adapter(codelldb, library_path),
+          }
         end
-        opts.dap = {
-          adapter = require("rustaceanvim.config").get_codelldb_adapter(codelldb, library_path),
-        }
       end
       vim.g.rustaceanvim = vim.tbl_deep_extend("keep", vim.g.rustaceanvim or {}, opts or {})
     end,
