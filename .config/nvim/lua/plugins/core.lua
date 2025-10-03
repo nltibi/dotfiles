1fbe4eff08536560c841c2e7f01dc67bc292e844
Tibi Nagy <nltibi@users.noreply.github.com>
2025-10-04 02:47:02 +0300

    feat(nvim): Close on :q if only one file is open

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -42,6 +42,7 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
+      close_if_last_window = true,
       filesystem = {
         filtered_items = {
           hide_dotfiles = false,
