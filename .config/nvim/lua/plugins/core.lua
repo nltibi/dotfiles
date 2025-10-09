745e123607fe7600680b73cbe279d76b02ee9394
Tibi Nagy <nltibi@users.noreply.github.com>
2025-10-10 02:26:16 +0300

    chore(nvim): Move UI customisations to a dedicated file

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -6,20 +6,6 @@ return {
     },
   },
   {
-    "catppuccin/nvim",
-    opts = {
-      -- transparent_background = true,
-      highlight_overrides = {
-        all = function(colors)
-          return {
-            -- SnacksIndentScope = { fg = colors.surface2 },
-            IblScope = { fg = colors.overlay0 },
-          }
-        end,
-      },
-    },
-  },
-  {
     "nvim-mini/mini.surround",
     version = "*",
     config = function()
@@ -45,62 +31,5 @@ return {
     config = function()
       require("mini.ai").setup()
     end,
-  },
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    opts = {
-      close_if_last_window = true,
-      filesystem = {
-        filtered_items = {
-          hide_dotfiles = false,
-          hide_by_name = { ".git" },
-        },
-      },
-      default_component_configs = {
-        modified = {
-          symbol = " ",
-        },
-        git_status = {
-          symbols = {
-            added = " ",
-            deleted = " ",
-            renamed = " ",
-            modified = " ",
-            staged = " ",
-            unstaged = "",
-            ignored = " ",
-            untracked = " ",
-            conflict = "󰳤 ",
-          },
-        },
-      },
-    },
-  },
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    ---@module "ibl"
-    ---@type ibl.config
-    opts = {
-      indent = {
-        char = "┊",
-      },
-      scope = {
-        enabled = true,
-        char = "┊",
-        show_start = false,
-        show_end = false,
-        show_exact_scope = true,
-      },
-    },
-  },
-  {
-    "folke/snacks.nvim",
-    ---@type snacks.Config
-    opts = {
-      indent = {
-        enabled = false,
-      },
-    },
   },
 }
